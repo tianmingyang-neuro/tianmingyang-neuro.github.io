19f4a2eae5d566597e8ac63a7d533bea352f4e31
tianmingyang-neuro <tyang2007@gmail.com>
2026-05-23 22:29:15 +0800

    add links to people who have them

--- data/people-data.js
+++ data/people-data.js
@@ -9,38 +9,38 @@ const PI = {
 
 // initials, name, role, blurb (optional), email (optional)
 const MEMBERS = [
-  { initials: 'RS', name: 'Ruoguang Si • 姒若光',  role: 'Postdoctoral Fellow', blurb: 'Hierarchical Cognition in humans' },
-  { initials: 'RZ', name: 'Rui Zhu • 朱瑞',    role: 'Graduate Student',    blurb: 'High-level cognition in humans' },
-  { initials: 'YH', name: 'Yuxi Huang • 黄宇熙',    role: 'Graduate Student',    blurb: 'Value coding and decision-making in PFC' },
-  { initials: 'PL', name: 'Peifan Li • 李沛繁',role: 'Graduate Student',    blurb: 'Neural Mechanisms of decision-making in PFC' },
-  { initials: 'YF', name: 'Yuhan Fu • 傅宇涵',role: 'Graduate Student',    blurb: 'Pac-Man games in monkeys' },
-  { initials: 'YF', name: 'Yunwei Li • 李昀蔚',role: 'Graduate Student',    blurb: 'Pac-Man games in monkeys' },
-  { initials: 'HW', name: 'Hualei Wang • 王华磊',    role: 'Graduate Student',    blurb: 'Neuroeconomics' },
-  { initials: 'HS', name: 'Hangyu Si • 司航宇',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC' },
-  { initials: 'YH', name: 'Yu He • 何昱',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC' },
-  { initials: 'ZW', name: 'Zhouying Wu • 伍周颖',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC' },
-  { initials: 'HZ', name: 'Zhihua Zhu • 竺智华',    role: 'Graduate Student',    blurb: ' Computational modeling of planning' },
-  { initials: 'PY', name: 'Puzhi Yu • 郁朴智',    role: 'Graduate Student',    blurb: ' Computational modeling and AI' },
-  { initials: 'MM', name: 'Ming Ma • 马铭',    role: 'Graduate Student',    blurb: 'Brain-inspired AI' },
-  { initials: 'WK', name: 'Wei Kong • 孔维',role: 'Lab Technician',    blurb: '' },
-  { initials: 'LY', name: 'Lu Yu • 	余璐',role: ' Lab Manager',    blurb: '' }
+  { initials: 'RS', name: 'Ruoguang Si • 姒若光',  role: 'Postdoctoral Fellow', blurb: 'Hierarchical Cognition in humans', url:'' },
+  { initials: 'RZ', name: 'Rui Zhu • 朱瑞',    role: 'Graduate Student',    blurb: 'High-level cognition in humans', url:'' },
+  { initials: 'YH', name: 'Yuxi Huang • 黄宇熙',    role: 'Graduate Student',    blurb: 'Value coding and decision-making in PFC', url:'https://yuxi-huang-ahey.github.io/' },
+  { initials: 'PL', name: 'Peifan Li • 李沛繁',role: 'Graduate Student',    blurb: 'Neural Mechanisms of decision-making in PFC', url:'' },
+  { initials: 'YF', name: 'Yuhan Fu • 傅宇涵',role: 'Graduate Student',    blurb: 'Pac-Man games in monkeys', url:'' },
+  { initials: 'YF', name: 'Yunwei Li • 李昀蔚',role: 'Graduate Student',    blurb: 'Pac-Man games in monkeys', url:'' },
+  { initials: 'HW', name: 'Hualei Wang • 王华磊',    role: 'Graduate Student',    blurb: 'Neuroeconomics', url:'' },
+  { initials: 'HS', name: 'Hangyu Si • 司航宇',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC', url:'' },
+  { initials: 'YH', name: 'Yu He • 何昱',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC', url:'' },
+  { initials: 'ZW', name: 'Zhouying Wu • 伍周颖',    role: 'Graduate Student',    blurb: 'Neural mechanisms of planning in PFC', url:'' },
+  { initials: 'HZ', name: 'Zhihua Zhu • 竺智华',    role: 'Graduate Student',    blurb: ' Computational modeling of planning', url:'' },
+  { initials: 'PY', name: 'Puzhi Yu • 郁朴智',    role: 'Graduate Student',    blurb: ' Computational modeling and AI', url:'' },
+  { initials: 'MM', name: 'Ming Ma • 马铭',    role: 'Graduate Student',    blurb: 'Brain-inspired AI', url:'https://mbjinx.github.io/' },
+  { initials: 'WK', name: 'Wei Kong • 孔维',role: 'Lab Technician',    blurb: '', url:'' },
+  { initials: 'LY', name: 'Lu Yu • 	余璐',role: ' Lab Manager',    blurb: '', url:'' }
   
 ];
 
 const ALUMNI = [
-  { name: 'Yang Xie',  year: '2013',  destination: 'PI, SJTU' },
-  { name: 'Ruiding Han',  year: '2013',  destination: '' },
-  { name: 'Yuanfeng Zhang',  year: '2013',  destination: '' },
-  { name: 'Yilin Chen',  year: '2013',  destination: 'Postdoc, ION' },
-  { name: 'Zhewei Zhang',  year: '2014',  destination: 'Postdoc, NIH' },
-  { name: 'Zhongqiao Lin',  year: '2014',  destination: 'Postdoc, ION' },
-  { name: 'Chechang Nie',  year: '2014',  destination: 'Postdoc, ION' },
-  { name: 'Siwei Guan',  year: '2015',  destination: 'Industrial' },
-  { name: 'Wenyi Zhang',  year: '2016',  destination: 'Postdoc, NYU' },
-  { name: 'Yunxian Bai',  year: '2017',  destination: 'Industrial' },
-  { name: 'Ruixin Su',  year: '2018',  destination: 'Industrial' },
-  { name: 'Pengyu Bao',  year: '2018',  destination: '' },
-  { name: 'Xiyuan Chen',  year: '2018',  destination: 'Industrial' },
-  { name: 'Xiaodong Li',  year: '2019',  destination: 'Postdoc, ION' },
-  { name: 'Qianli Yang',  year: '2019',  destination: 'PI, ECNU' }
+  { name: 'Yang Xie',  year: '2013',  destination: 'PI, SJTU', url:'https://yxie-lab.com' },
+  { name: 'Ruiding Han',  year: '2013',  destination: '', url:'' },
+  { name: 'Yuanfeng Zhang',  year: '2013',  destination: '' , url:''},
+  { name: 'Yilin Chen',  year: '2013',  destination: 'Postdoc, ION', url:'' },
+  { name: 'Zhewei Zhang',  year: '2014',  destination: 'Postdoc, NIH', url:'https://www.zhewei-zhang.com/' },
+  { name: 'Zhongqiao Lin',  year: '2014',  destination: 'Postdoc, ION', url:'' },
+  { name: 'Chechang Nie',  year: '2014',  destination: 'Postdoc, ION', url:'' },
+  { name: 'Siwei Guan',  year: '2015',  destination: 'Industrial', url:'' },
+  { name: 'Wenyi Zhang',  year: '2016',  destination: 'Postdoc, NYU' , url:''},
+  { name: 'Yunxian Bai',  year: '2017',  destination: 'Industrial', url:'' },
+  { name: 'Ruixin Su',  year: '2018',  destination: 'Industrial', url:'' },
+  { name: 'Pengyu Bao',  year: '2018',  destination: '', url:'' },
+  { name: 'Xiyuan Chen',  year: '2018',  destination: 'Industrial', url:'' },
+  { name: 'Xiaodong Li',  year: '2019',  destination: 'Postdoc, ION', url:'' },
+  { name: 'Qianli Yang',  year: '2019',  destination: 'PI, ECNU', url:'https://faculty.ecnu.edu.cn/_s9/yql2/main.psp' }
 ];
